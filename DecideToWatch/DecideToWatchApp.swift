e534b920391203d7467bd03490583ee00f75df53
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2026-04-17 17:24:27 -0400

    Initial Commit

---
+++ DecideToWatch/DecideToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecideToWatchApp.swift
+//  DecideToWatch
+//
+//  Created by Bridge Informatics on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DecideToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
